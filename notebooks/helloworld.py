afd6f050709a750672cc9b39435da3f300c0e041
ozak <ozak@tutopia.com>
2014-03-05 14:37:10 -0600

    Version 1.0

---
+++ notebooks/helloworld.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+My First script in Python
+
+Author: Me
+E-mail: me@me.com
+Website: http://me.com
+GitHub:  https://github.com/me
+Date: Today
+
+This code computes Random Walks and graphs them
+'''
+
+'''
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+'''
+print 'Hello World!'
